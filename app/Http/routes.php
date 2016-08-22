d3ddf54fa1bf1aa24a3a6b8cecb62aa618b1ce18
Jilvan Pinheiro de Souza Junior <jilvan.souza@usp.br>
2016-08-22 02:09:30 -0300

    Searchbar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -109,7 +109,7 @@ Route::get('/pessoas', function(){
 });
 
 Route::post('/pessoas/pesquisar/', function( Request $request){
-    $researchers = Researcher::where( 'name', 'like', $request->searchbar)->orWhere('bio','like', $request->searchbar)->get();
+    $researchers = Researcher::where( 'name', 'like', '%'.$request->searchbar.'$')->orWhere('bio','like',  '%'.$request->searchbar.'$')->get();
 
     return view('People.pessoas', [
         'researchers' => $researchers
@@ -130,7 +130,7 @@ Route::get('/producoes', function(){
 });
 
 Route::post('/producoes/pesquisar', function( Request $request){
-    $papers = Paper::where( 'name', 'like', $request->searchbar)->orWhere('fields','like', $request->searchbar)->orWhere('summary','like',$request->searchbar)->orWhere('authors','like',$request->searchbar)->get();
+    $papers = Paper::where( 'name', 'like',  '%'.$request->searchbar.'$')->orWhere('fields','like',  '%'.$request->searchbar.'$')->orWhere('summary','like', '%'.$request->searchbar.'$')->orWhere('authors','like', '%'.$request->searchbar.'$')->get();
 
     return view('Productions.producoes', [
         'papers' => $papers
