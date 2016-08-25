07440f6d28e95a1ac0463742f017f67e14466456
Jilvan Pinheiro de Souza Junior <jilvan.souza@usp.br>
2016-08-25 02:45:07 -0300

    SEO

--- resources/views/People/pessoas.blade.php
+++ resources/views/People/pessoas.blade.php
@@ -8,43 +8,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Pessoas | Centro de Neuromodulação</title>
-    <meta name="description" content="">
+    <meta name="description" content="O Centro de Neuromodulação da EPM-UNIFESP é uma iniciativa científico-tecnológica voltada ao desenvolvimento de métodos e estudos em psiquiatria e áreas afins.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="referrer" content="origin">
-    <meta property="fb:app_id" content="803180099766052">
-    <meta property="og:site_name" content="Johns Hopkins University">
-    <meta property="og:title" content="University Leadership | Johns Hopkins University">
-    <meta property="og:url" content="index.html">
-    <meta property="og:description" content="Johns Hopkins University's deans, senior administrators, and the cabinet of Ronald J. Daniels, who became the university's 14th president in March 2009">
-    <meta property="og:image" content="../../assets/uploads/2014/08/65-1600x800.jpg">
-    <meta property="og:image:url" content="../../assets/uploads/2014/08/65-1600x800.jpg">
-    <link rel="apple-touch-icon" sizes="57x57" href="../../theme/images/favicons/apple-touch-icon-57x57-9e41f838fa.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="../../theme/images/favicons/apple-touch-icon-60x60-61eced9b63.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="../../theme/images/favicons/apple-touch-icon-72x72-03d872d9f6.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="../../theme/images/favicons/apple-touch-icon-76x76-4f7266e637.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="../../theme/images/favicons/apple-touch-icon-114x114-af3f9d754d.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="../../theme/images/favicons/apple-touch-icon-120x120-8f80bcbb5b.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="../../theme/images/favicons/apple-touch-icon-144x144-4d53b498e3.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="../../theme/images/favicons/apple-touch-icon-152x152-b0c947da76.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="../../theme/images/favicons/apple-touch-icon-180x180-b3415e4427.png">
-    <link rel="icon" type="image/png" href="../../theme/images/favicons/favicon-32x32-16d864abff.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="../../theme/images/favicons/favicon-194x194-52fce67c45.png" sizes="194x194">
-    <link rel="icon" type="image/png" href="../../theme/images/favicons/favicon-96x96-165f54371d.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="../../theme/images/favicons/android-chrome-192x192-ca6391956e.png" sizes="192x192">
-    <link rel="icon" type="image/png" href="../../theme/images/favicons/favicon-16x16-3c015a0795.png" sizes="16x16">
-    <link rel="manifest" href="../../theme/images/favicons/manifest-8743dc857b.json">
-    <link rel="mask-icon" href="../../theme/images/favicons/safari-pinned-tab-551917f6c3.svg" color="#002d72">
-    <meta name="apple-mobile-web-app-title" content="Johns Hopkins University">
-    <meta name="application-name" content="Johns Hopkins University">
+
+
+    <meta property="og:site_name" content="Centro de Neuromodulação">
+    <meta property="og:title" content="Pessoas | Centro de Neuromodulação">
+    <meta property="og:url" content="{{url('/')}}">
+    <meta property="og:description" content="O Centro de Neuromodulação da EPM-UNIFESP é uma iniciativa científico-tecnológica voltada ao desenvolvimento de métodos e estudos em psiquiatria e áreas afins.">
+
+
+    <meta property="og:image" content="{{asset('/images/logo_azul.png')}}">
+    <meta property="og:image:url" content="{{asset('/images/logo_azul.png')}}">
+
+    <!--trocar todas as imagens-->
+    <link rel="apple-touch-icon" sizes="57x57" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/images/logo_azul.png')}}">
+    <link rel="icon" type="image/png" href="{{asset('/images/logo_azul.png')}}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{asset('/images/logo_azul.png')}}" sizes="194x194">
+    <link rel="icon" type="image/png" href="{{asset('/images/logo_azul.png')}}" sizes="96x96">
+    <link rel="icon" type="image/png" href="{{asset('/images/logo_azul.png')}}" sizes="192x192">
+    <link rel="icon" type="image/png" href="{{asset('/images/logo_azul.png')}}" sizes="16x16">
+    <link rel="icon" href="{{asset('/images/logo_azul.png')}}">
+    <meta name="apple-mobile-web-app-title" content="Centro de Neuromodulação EPM-UNIFESP">
+    <meta name="application-name" content="Centro de Neuromodulação EPM-UNIFESP">
     <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="msapplication-TileImage" content="../../theme/images/favicons/mstile-144x144-8b217b2343.png">
-    <meta name="theme-color" content="#ffffff">
-    <meta name="keywords" content="administration">
-    <link rel="https://api.w.org/" href="../../wp-json/index.html">
-    <link rel="canonical" href="index.html">
-    <script>
-        window.programExplorerUrl = "../../academics/index.html";
-    </script>
+    <meta name="msapplication-TileImage" content="{{asset('/images/logo_azul.png')}}">
+    <meta name="theme-color" content="#b2d1d3">
+
+
+
+    <link rel="https://api.w.org/" href="wp-json/index.html">
+    <link rel="canonical" href="{{url('/')}}">
+
     <script src="{{asset('/js/modernizr-acd544d837.js')}}"></script>
     <script src="{{asset('/js/head-c1049261eb.js')}}"></script>
     <link rel="stylesheet" href="{{asset('/css/base-e1f1a1c0a0.css')}}">
